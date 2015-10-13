0c82b4422230ed5d1084e055010e496aff0696e3
Kool Havis <havisk@gmail.com>
2015-10-12 22:03:36 -0400

    finished

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,19 @@
 
 (function () {
 
-  console.log('It Works!');
+  var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=free&includes=Images,Shop';
+  var templateString = $('#itemTemplate').text();
+  var templateFunction = _.template(templateString);
+
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  })
+  .then(function (response) {
+    _.each(response.results, function (item) {
+      var itemHTML = templateFunction(item);
+      $('.container').append(itemHTML);
+    });
+  });
 })();
